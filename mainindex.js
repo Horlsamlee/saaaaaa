170f2844627f7c3d7fdbbae08b3ec9c3309d8c95
Horlsamlee <shorlonade@gmail.com>
2024-02-28 11:50:20 -0600

    horlsam

---
+++ mainindex.js
@@ -0,0 +1,45 @@
+﻿class Olonade{
+    constructor(
+        fid,
+        name,
+        age,
+        position,
+        location,
+        skinColor,
+        maritalStatus,
+        childrenG,
+        childrenB,
+        dateMarried
+
+    ){
+        this.fid = fid;
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.location = location;
+        this.skinColor = skinColor;
+        this.maritalStatus = maritalStatus;
+        this.children = {
+            girl : childrenG,
+            boy : childrenB
+        };
+        this.dateMarried = dateMarried;
+    }
+       married(marital){
+        this.maritalStatus = marital;
+       }
+        newChildren(girlChild, boyChild){
+            this.children.girl = girlChild;
+            this.children.boy = boyChild;
+        }
+        totalMarriedDays(){  
+            let dateOfMarriage = new Date(this.dateMarried); 
+            let todaysDate = new Date();
+                let  daysMarried = todaysDate - dateOfMarriage;
+                let  tDaysMarried = Math.floor((daysMarried/(1000 * 3600 * 24))/365);
+                return tDaysMarried;                       
+            };
+           
+}
+
+export default Olonade
---
+++ secondfl.js
@@ -0,0 +1,87 @@
+﻿import Olonade from "./mainindex.js"
+
+const yusufoy = new Olonade(
+    "O01",
+    "Yusuf Olonade",
+    35 + " years",
+    "first born",
+    "osun state, Nigeria",
+    "brown",
+    "married",
+    1,
+    1,
+    "september 2, 2018",
+)
+
+const tbal = new Olonade(
+    "O02",
+    "Oluwatoyin Olonade",
+    33 + " years",
+    "second born",
+    "ibadan, Nigeria",
+    "light yellowish-brown",
+    "married",
+    1,
+    "none",
+    "january 6, 2023",
+
+)
+
+const ib = new Olonade(
+    "O03",
+    "Ibrahim Olonade",
+    31 + " years",
+    "Third born",
+    "lagos, Nigeria",
+    "light brown",
+    "married",
+    1,
+    1,
+    "october 21, 2019",
+)
+
+const salam = new Olonade(
+    "O04",
+    "Abdulsalam Olonade",
+    25 + " years",
+    "last of four",
+    "Famagusta Cyprus",
+    "light yellowish-brown",
+    "single",
+    "none",
+    "none",
+    undefined
+)
+
+const targetArr = [yusufoy, tbal, ib, salam];
+const form = document.getElementById('formspace');
+const result = document.getElementById('result');
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    const inputElement = document.getElementById('myInput').value
+    for (let i =0; i<targetArr.length; i++){
+        const element = targetArr[i];
+        const elementName = element.name
+        if(elementName.toLowerCase() === inputElement.toLowerCase()){
+            result.innerHTML= 
+            `
+               <ul>
+                   <li><span class ="listspan">Name:</span> ${element.name}</li>
+                   <li><span class ="listspan">Age:</span> ${element.age}</li>
+                   <li><span class ="listspan">Position:</span> ${element.position}</li>
+                   <li><span class ="listspan">complexion:</span> ${element.skinColor}</li> 
+                   <li><span class ="listspan">Current Location:</span> ${element.location}</li>
+                   <li><span class ="listspan">Marital Status:</span> ${element.maritalStatus}</li>
+                   <li><span class ="listspan">Children:</span> ${element.newChildren()}</li>
+                   <li><span class ="listspan">Date married:</span> ${element.dateMarried}</li>
+               </ul>               
+            `
+        ;
+        return;
+        }
+    }
+    result.textContent = "No matching information found"
+})
+//console.log(tbal.totalMarriedDays())
+//console.log(salam)
+//console.log("married for :", salam.totalMarriedDays() + " years")
